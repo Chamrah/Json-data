d7858b7534527ae02fc57dfe72b78a46defb294f
Ziad Chamrah <147912275+Chamrah@users.noreply.github.com>
2023-12-20 20:42:26 +0100

    Add files via upload

---
+++ Json/data.py
@@ -0,0 +1,17 @@
+import json
+data_open = open("data.json","r")
+x = json.loads(data_open.read())
+print(x)
+
+
+
+data_write ={  
+    "langage":"python"}
+file = open("data.json","a")
+file.write(json.dumps(data_write))
+file.close()
+
+
+
+
+
